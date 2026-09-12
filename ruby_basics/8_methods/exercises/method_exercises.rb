eb4c7ca3ded50396f2de98cfc2ed71c151c28ac7
Vasile Marin <vasile.marin@generationhome.com>
2026-09-12 14:22:52 +0100

    add sorbet for typechecking
    complete exercises for chapter 8 methods

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -1,3 +1,7 @@
+# typed: strict
+require 'sorbet-runtime'
+extend T::Sig
+#
 # Since lesson #8 is on methods, you will be writing the entire method.
 # To gain more familiarity, look up the documentation for each hint.
 # Remember to unskip the corresponding tests one at a time.
@@ -7,23 +11,39 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+sig { params(number: Integer).returns(String) }
+def ascii_translator(number)
+  number.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
+sig { params(current_sports: T::Array[String], favorite_sports: T::Array[String]).returns(T::Array[String]) }
+def common_sports(current_sports, favorite_sports)
+  current_sports.intersection(favorite_sports)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#uniq and Array#sort together
 
+sig { params(games: T::Array[String]).returns(T::Array[String]) }
+def alphabetical_list(games)
+  games.sort.uniq
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
 
+sig { params(number: Integer).returns(String) }
+def lucky_number(number = 7)
+  "Today's lucky number is #{number}"
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
@@ -31,6 +51,11 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+sig { params(character: String).returns(T.any(Integer, String)) }
+def ascii_code(character)
+  if character.length != 1 then return 'Input Error' end
+  character.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -40,9 +65,24 @@
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+sig { params(animal: String).returns(NilClass) }
+def pet_pun(animal)
+  case animal
+  when 'cat'
+    puts 'Cats are purr-fect!'
+  when 'dog'
+    puts 'Dogs are paw-some!'
+  else
+    puts "I think #{animal}s have pet-tential!"
+  end
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+sig { params(year: Integer).returns(T::Boolean) }
+def twenty_first_century?(year)
+  year.between?(2001, 2100)
+end
