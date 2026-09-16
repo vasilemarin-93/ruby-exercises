2a3d25cc1d7487ecbf4eb573c3ed195fd9373b75
Vasile Marin <vasile.marin@generationhome.com>
2026-09-16 11:16:46 +0100

    completed nested_array and nested_hash exercises

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -1,3 +1,10 @@
+# typed: true
+
+require 'sorbet-runtime'
+extend T::Sig
+
+
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]], language_name: Symbol).returns(T.untyped) }
 def find_language_information(languages, language_name)
   # Take languages (a nested hash) and language_name as a symbol, return the
   # value for the language_name key (which will be another hash!)
@@ -7,33 +14,51 @@ def find_language_information(languages, language_name)
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
   # }
+  languages.dig(language_name)
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]], language_name: Symbol, info_key: Symbol, info_value: T.any(String, T::Boolean)).returns(T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]) }
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
   # hash of language_name, then return the updated languages hash
+  T.must(languages[language_name])[info_key] = info_value
+  languages
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]], language_name: Symbol, language_info_value: T::Hash[Symbol, T.any(String, T::Boolean)]).returns(T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]) }
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
   # to it, then return languages
+  languages[language_name] = language_info_value
+  languages
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]], language_name: Symbol, info_key: Symbol).returns(T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]) }
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
+  T.must(languages[language_name]).delete(info_key)
+  languages
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]], language_name: Symbol).returns(T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]) }
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
+  languages.delete(language_name)
+  languages
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]).returns(T::Hash[Symbol, T::Hash[Symbol, T.any(String, T::Boolean)]]) }
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+  languages.select do |lang, data|
+    data[:is_beautiful?] == true
+  end
 end
 
+sig { params(languages: T::Hash[Symbol, T::Hash[Symbol, T.untyped]], language_name: T.untyped, fact_index: T.untyped).returns(T.untyped) }
 def find_language_facts(languages, language_name, fact_index = 0)
   # Take languages (now with additional facts added to each language with the
   # key :facts and value of an array of strings) and return the fact
@@ -50,4 +75,5 @@ def find_language_facts(languages, language_name, fact_index = 0)
   #                 initial_release: 'December 4, 1995',
   #                 is_beautiful?: false }
   # }
+  languages.dig(language_name, :facts, fact_index)
 end
